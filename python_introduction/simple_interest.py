11d7222c8a20c67e0d99dc14ff45831a1bae5b45
Theo-Dorh <kwameadjanor26@gmail.com>
2024-12-15 22:38:57 +0000

    Update simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Defining the variables
+principal = 1000  # Initial investment ($1000)
+rate = 0.05       # Annual interest rate (5%)
+time = 3          # Time in years (3 years)
+
+# Calculating the the simple interest
+interest = principal * rate * time
+
+# Printing the result to the console
+print(f"The simple interest is: {interest}")
